185401707afa18925d8b1f64e64739ddba841819
vipin <vipin@stackmod.io>
2023-08-04 06:17:25 +0000

    printing exercise 1

---
+++ printing/print-exercise-1.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * program to print the pattern for a given number rows
+ * 
+ * 1
+ * 12
+ * 123
+ * 1234
+ * 123
+ * 12
+ * 1
+ * 
+ * @author me.vipin22@gmail.com
+ */
+
+// $rows = 20;
+$rows = (int)readline('Enter the number of rows you need to print: ');
+
+for ($i = 1; $i <= $rows; $i++) {
+    if ($i <= ($rows/2)) {
+        for($j = 0; $j <= ($i - 1); $j++) {
+            printf("%d", $j+1);
+        }
+    } else {
+        for($j = 0; $j <= ($rows % $i); $j++) {
+            printf("%d", $j + 1);
+        }
+    }
+
+    printf("\n"); 
+}
